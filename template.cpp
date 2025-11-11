35eac7e896f09a4827ea58cf8f7a3eadfba19e2d
Kexuan Miao <kmiao@ualberta.ca>
2025-11-11 00:18:37 -0700

    Added coding template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+// Author: Kexuan Miao
+#include <bits/stdc++.h>
+using namespace std;
+
+#define cmax(u, v) u = max(u, v)
+#define cmin(u, v) u = min(u, v)
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+
+const ll m = 1e9 + 7;
+
+void solve(){
+    
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
